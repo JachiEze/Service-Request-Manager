ae4159f8d68a5628e84fa78b3a62c475fe3c9856
Jachimike Divine Ezenwamadu <94161891+JachiEze@users.noreply.github.com>
2025-09-26 09:47:48 +0100

    Update LoginViewModel.cs

--- Models/LoginViewModel.cs
+++ Models/LoginViewModel.cs
@@ -4,15 +4,12 @@ namespace ServiceRequestForm.Models
 {
     public class LoginViewModel
     {
-
-
-        [Required]
+        [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
-        public string Username { get; set; }
+        public string? Username { get; set; }
 
-        public string Password { get; set; }
-
-
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public string? Password { get; set; }
     }
 }
-
